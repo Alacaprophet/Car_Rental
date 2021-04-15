6d516e9a23f748673fa746dd8b76ff13358e4e4b
Alacaprophet <66279956+Alacaprophet@users.noreply.github.com>
2021-04-15 20:43:23 +0300

    Adding entities.

---
+++ src/Domain/Entities/VehicleModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace Domain.Entities
+{
+    class VehicleModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [Required]
+        public int VehicleBrandId { get; set; }
+        public VehicleBrand VehicleBrand { get; set; }
+    }
+}
