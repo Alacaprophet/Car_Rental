d19e0bbf0eab02e80b6457ed9d274bfcc76b3aa8
Alacaprophet <66279956+Alacaprophet@users.noreply.github.com>
2021-04-16 18:06:45 +0300

    Creating persistence layer

--- src/Domain/Entities/VehicleClassType.cs
+++ src/Domain/Entities/VehicleClassType.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 namespace Domain.Entities
 {
-   public class VehicleClassType
+    public class VehicleClassType
     {
         [Key]
         public int Id { get; set; }
--- src/Domain/Entities/VehicleModel.cs
+++ src/Domain/Entities/VehicleModel.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 namespace Domain.Entities
 {
-    class VehicleModel
+    public class VehicleModel
     {
         [Key]
         public int Id { get; set; }
