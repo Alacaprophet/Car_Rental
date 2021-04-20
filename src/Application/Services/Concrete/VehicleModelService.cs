0e460bc283e578bd447209e1a2068b05b7af5618
Alacaprophet <66279956+Alacaprophet@users.noreply.github.com>
2021-04-20 20:54:39 +0300

    Retry

---
+++ src/Application/Services/Concrete/VehicleModelService.cs
@@ -0,0 +1,56 @@
+﻿using Application.Infrastructure.Persistence;
+using Application.Services.Common;
+using Domain.DTOs;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services.Concrete
+{
+    public class VehicleModelService :BaseService, IVehicleModelService
+    {
+        public VehicleModelService(ICarRentalDbContext context) : base(context) { }
+        public Response Add(VehicleModel vehicleModel)
+        {
+            Context.VehicleModel.Add(vehicleModel);
+            Context.SaveChanges();
+            return Response.Succes("Ekleme Başarılı");
+        }
+
+        public Response Delete(int id)
+        {
+            var DeleteToModel = GetById(id);
+            Context.VehicleModel.Remove(DeleteToModel);
+            Context.SaveChanges();
+            return Response.Succes("Başarılı bir şekilde silindi");
+        }
+
+        public List<VehicleModel> Get(VehicleModelFilter filter)
+        {
+            var items = (from m in Context.VehicleModel
+                         orderby m.Name
+                         select m).ToList();
+            var items1 = (from vm in Context.VehicleModel
+                          join vb in Context.VehicleBrand on vm.VehicleBrandId equals vb.Id
+                          select new {vb.Name} );//select new {vb.Name,vm.Name}
+            return items;
+        }
+
+        public VehicleModel GetById(int id)
+        {
+            return Context.VehicleModel.Where(m => m.Id == id).FirstOrDefault();
+        }
+
+        public Response Update(VehicleModel vehicleModel)
+        {
+            var vehicleModelToUpdate = GetById(vehicleModel.Id);
+            vehicleModelToUpdate.Name = vehicleModel.Name;
+            vehicleModelToUpdate.VehicleBrand = vehicleModel.VehicleBrand;
+            Context.SaveChanges();
+            return Response.Succes("Güncelleme Başarılı");
+        }
+    }
+}
