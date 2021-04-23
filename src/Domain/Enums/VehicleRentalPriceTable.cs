b107783192ea74c68ea4c4a3830dab92ed27dad7
Alacaprophet <66279956+Alacaprophet@users.noreply.github.com>
2021-04-23 14:28:14 +0300

    Adding VEhicleRentalPriceService . Create vehicle rental price index page.

---
+++ src/Domain/Enums/VehicleRentalPriceTable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Enums
+{
+     public  class VehicleRentalPriceTable
+    {
+        public int RentalPriceId { get; set; }
+        public string ModelName { get; set; }
+        public string TransmissionName { get; set; }
+        public string PeriodName { get; set; }
+        public string ColorName { get; set; }
+        public int ProductYear { get; set; }
+        public Decimal Fee { get; set; }
+    }
+}
